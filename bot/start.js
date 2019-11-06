df326b1e7e96b0abdb2eb5da6aee81645a4d2a27
ErKiran <kiruu1238@gmail.com>
2019-11-07 00:38:30 +0545

    Changed Blocking code

--- bot/start.js
+++ bot/start.js
@@ -2,24 +2,28 @@ const { bot } = require('./bot');
 const keyboard = require('../keyboard');
 const User = require('../models/user');
 const { sendMessage } = require('./sendMessage');
+const { logger } = require('../logs');
 
 
 bot.onText(/\/start/, async msg => {
-    const imageRequest = await bot.getUserProfilePhotos(754019213);
-    console.log(imageRequest)
-    const newUser = new User({
-        telegram_id: msg.chat.id,
-        first_name: msg.chat.first_name,
-        last_name: msg.chat.last_name,
-        language_code: msg.from.language_code,
-        created: Date.now()
-    })
-    const check = await User.find({ telegram_id: msg.chat.id });
+    try {
+        const newUser = new User({
+            telegram_id: msg.chat.id,
+            first_name: msg.chat.first_name,
+            last_name: msg.chat.last_name,
+            language_code: msg.from.language_code,
+            created: Date.now()
+        })
+        const check = await User.find({ telegram_id: msg.chat.id });
 
-    if ((check.length == 0)) {
-        await newUser.save();
+        if ((check.length == 0)) {
+            await newUser.save();
+        }
+        bot.sendMessage(msg.chat.id, `Welcome`, { reply_markup: keyboard.startkeyboard })
     }
-    bot.sendMessage(msg.chat.id, `Welcome`, { reply_markup: keyboard.startkeyboard })
+    catch (e) {
+        logger.error(e, msg.chat.id)
+    }
 })
 
 bot.onText(/\/text/, async msg => {
